2391f369638336b8d159580edb737fa396cbbb4a
PTG1326 <f20212085@goa.bits-pilani.ac.in>
2022-07-17 18:48:01 +0530

    rev pentagon rotation

--- Assets/Scripts/Rotate_Pentagon.cs
+++ Assets/Scripts/Rotate_Pentagon.cs
@@ -20,13 +20,34 @@ public class Rotate_Pentagon : MonoBehaviour
             count += rotationAmount;
             yield return new WaitForSeconds(delaySpeed);
     }
-            player.AddForce((new Vector2(50 , 50)), ForceMode2D.Impulse) ;
+    
+            player.AddForce((new Vector2(35 , 35)), ForceMode2D.Impulse) ;
             Debug.Log("hello");
             player.gravityScale = 5;
             player.rotation = 0;
             
             
     }
+
+    public IEnumerator SlowSpinRev(){
+        float count = 0;
+        while(count <= 72){
+            rb.transform.RotateAround(anchor.transform.localPosition, Vector3.forward, -rotationAmount);
+            player.transform.RotateAround(anchor.transform.localPosition, Vector3.forward, -rotationAmount);
+            player.gravityScale = 0;
+            player.velocity = new Vector2(0,0);
+            count += rotationAmount;
+            yield return new WaitForSeconds(delaySpeed);
+    }
+    
+            player.AddForce((new Vector2(-35 , 35)), ForceMode2D.Impulse) ;
+            Debug.Log("hello");
+            player.gravityScale = 5;
+            player.rotation = 0;
+            
+            
+    }
+
     
     
 }
--- Assets/Scripts/Rotation_trigger.cs
+++ Assets/Scripts/Rotation_trigger.cs
@@ -16,10 +16,19 @@ public class Rotation_trigger : MonoBehaviour
     // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D cd)
     {
-        StartCoroutine(FindObjectOfType<Rotate_Pentagon>().SlowSpin());
+        if (rb.velocity.x > 0) {
+            StartCoroutine(FindObjectOfType<Rotate_Pentagon>().SlowSpin());
         // FindObjectOfType<PlayerController>().transform.position = new Vector2(-2466, (float)1057.9);
 
         Debug.Log("hello");
-        
+        }
+
+        else if (rb.velocity.x < 0) {
+            StartCoroutine(FindObjectOfType<Rotate_Pentagon>().SlowSpinRev());
+        // FindObjectOfType<PlayerController>().transform.position = new Vector2(-2466, (float)1057.9);
+
+        Debug.Log("hello");
+        }
+
     }
 }
